8df3d6f908c64d90814c2d6d8cadc16f4cc8b127
dominikbasista <dominikbasista@gmail.com>
2019-02-19 09:17:37 +0100

    dodano sciezki do urls oraz do orders.views. Utworzono forms

--- Shop/urls.py
+++ Shop/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
-    # path('order/', include('order.urls', namespace='order')),
+    path('order/', include('orders.urls', namespace='order')),
     path('', include('shop.urls', namespace='shop')),
 
 ]
