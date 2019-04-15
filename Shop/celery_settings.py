e773da65b31c8c525acf1933c30990ed7670c81b
Dominik <dominikbasista@gmail.com>
2019-04-15 23:38:15 +0200

    zainstalowano celery 4.0.0. Poprzednia wersja nie działała (problemy z importem Proxy)

---
+++ Shop/celery_settings.py
@@ -0,0 +1,12 @@
+import os
+from django.conf import settings
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Shop.settings')
+
+app = Celery('Shop')
+
+app.config_from('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
